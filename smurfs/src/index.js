c942fa31903ffb4b6e7bb2c4f8220d2bd04b2904
Robert Benavidez <murabitbenavidez@gmail.com>
2019-07-26 13:02:03 -0400

    intial reducer and action setup

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -3,4 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import  { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { reducer } from './store/reducers/index';
+//import thunk from 'redux-thunk';
+
+const store = createStore(reducer)
+
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>, document.getElementById("root"));
---
+++ smurfs/src/store/reducers/index.js
@@ -0,0 +1,18 @@
+
+
+const initialState = {
+    smurfs: [],
+    fetchingSmurf: false,
+    addingSmurf: false,
+    updatingSmurf: false,
+    deletingSmurf: false,
+    error: null
+}
+
+
+export const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        default: 
+        return state;
+    }
+};
