993f8428f5c7786cf797e20e913c0d4d770ad812
Robert Benavidez <murabitbenavidez@gmail.com>
2019-07-27 11:26:25 -0400

    mvp with some inline styling

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Smurfs from './Smurfs'
+import Form from './Form';
 
 import "./App.css";
 
@@ -14,20 +15,32 @@ const App = () => {
 
   useEffect(() => {
     const fetchSmurf = async () => {
-      const result = await axios('http://localhost:3333/smurfs');
-      setSmurfs(result.data)
+    const result = await axios('http://localhost:3333/smurfs');
+    setSmurfs(result.data)
     }
     fetchSmurf();
   }, []);
 
+  const addSmurf = (e, name, age, height) => {
+    const newSmurf = {
+      name: name, 
+      age: age,
+      height: height
+    }
+    e.preventDefault();
+      axios 
+        .post('http://localhost:3333/smurfs', newSmurf)
+        .then (res => {setSmurfs(res.data)})
+        .catch(err => {console.log(err.response)})
+  }
   
   
     return (
-      <context.Provider value={{smurfs}}>
+      <context.Provider value={{smurfs, addSmurf}}>
       <div className="App">
         <h1>Welcome to Smurf Village</h1>
+        <Form />
         <Smurfs smurfs={smurfs} />
-        
       </div>
       </context.Provider>
     );
--- smurfs/src/components/Smurfs.js
+++ smurfs/src/components/Smurfs.js
@@ -1,15 +1,22 @@
 import React, { useContext } from 'react';
-import { context } from './context'
+import { context } from './context';
 
-
+const card = {
+    display: 'inline-block', 
+    margin: '20px',
+    width: '250px',
+    backgroundColor: 'lightgrey',
+}
 
 const Smurfs = () => {
     const {smurfs} = useContext(context)
     return (
         smurfs.map(smurf =>{
             return (
-            <div key={smurf.id}>
-                <h2>{smurf.name}</h2>
+            <div key={smurf.id} style={card}>
+                <h2>Name - {smurf.name}</h2>
+                <h2>Age - {smurf.age}</h2>
+                <h2>Height - {smurf.height}</h2>
             </div>
             )
         })
